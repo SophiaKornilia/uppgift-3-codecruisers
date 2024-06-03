56290fe3f03d219dba88f4d9be286fb071edcce0
Jennika Elisson <jennika.elisson@gmail.com>
2024-06-03 22:59:56 +0200

    added initializing stripe file

---
+++ server/stripe.ts
@@ -0,0 +1,11 @@
+import Stripe from 'stripe';
+
+const initStripe = (): Stripe | undefined => {
+    const apiKey = process.env.STRIPE_KEY;
+    if (!apiKey) return;
+    return new Stripe(apiKey, {
+        apiVersion: '2024-04-10', // Uppdaterad till senaste versionen
+    });
+};
+
+export default initStripe;
